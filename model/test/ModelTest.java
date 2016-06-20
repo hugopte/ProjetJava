97930cb69b7964f0e7f757f0e93bebf8633e67f0
Hugo PETTE <Hugo PETTE@HugoPETTE-PC>
2016-06-20 22:01:16 +0200

    test model

---
+++ model/test/ModelTest.java
@@ -0,0 +1,36 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import model.Model;
+import model.Map;
+
+public class ModelTest {
+
+	@Before
+	public void setUp() throws Exception {
+		}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMap() {
+		Model model = new Model();
+		 Map testMap = new Map("C:/Users/Hugo PETTE/git/ProjetJava/model/images/map3.txt",model);
+	
+		int x=0, y=0;
+		for(y=0; y<12	; y++)
+		{
+			for(x=0; x<20; x++)
+			{
+				assertNotNull(testMap.getElem(y,x));
+			}
+		}
+	}
+
+}
